0e8033432a95d15fef64a8182b0636bfac6ec950
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-08-22 00:03:37 -0700

    save async and send message in chat when done

--- src/main/kotlin/org/waste/of/time/SaveCommand.kt
+++ src/main/kotlin/org/waste/of/time/SaveCommand.kt
@@ -2,7 +2,12 @@ package org.waste.of.time
 
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.context.CommandContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+import net.minecraft.client.MinecraftClient
+import net.minecraft.text.Text
 import java.io.File
 
 
@@ -10,10 +15,13 @@ object SaveCommand : Command<FabricClientCommandSource> {
     private val regionBasedStorage = CustomRegionBasedStorage(File("WorldTools/region").toPath(), false)
 
     override fun run(context: CommandContext<FabricClientCommandSource>?): Int {
-        regionBasedStorage.use { storage ->
-            WorldTools.cachedChunks.forEach { entry ->
-                storage.write(entry.value.pos, ClientChunkSerializer.serialize(entry.value))
+        CoroutineScope(Dispatchers.Default).launch {
+            regionBasedStorage.use { storage ->
+                WorldTools.cachedChunks.forEach { entry ->
+                    storage.write(entry.value.pos, ClientChunkSerializer.serialize(entry.value))
+                }
             }
+            MinecraftClient.getInstance().inGameHud.chatHud.addMessage(Text.of("Save completed"))
         }
         return 0
     }
