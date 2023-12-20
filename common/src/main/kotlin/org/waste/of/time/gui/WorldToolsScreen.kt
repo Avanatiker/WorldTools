7ced0c3466eb6a2044a705f9f71531a2237df9b0
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-12-19 22:05:31 -0800

    scale and position world name gui elements more sanely on varying screen sizes

--- common/src/main/kotlin/org/waste/of/time/gui/WorldToolsScreen.kt
+++ common/src/main/kotlin/org/waste/of/time/gui/WorldToolsScreen.kt
@@ -1,9 +1,7 @@
 package org.waste.of.time.gui
 
 import net.minecraft.client.gui.screen.Screen
-import net.minecraft.client.gui.widget.ButtonWidget
-import net.minecraft.client.gui.widget.TextFieldWidget
-import net.minecraft.client.gui.widget.TextWidget
+import net.minecraft.client.gui.widget.*
 import net.minecraft.text.Text
 import org.waste.of.time.CaptureManager
 import org.waste.of.time.CaptureManager.levelName
@@ -12,57 +10,30 @@ import org.waste.of.time.WorldTools.mm
 
 object WorldToolsScreen : Screen(BRAND.mm()) {
     override fun init() {
-        val title = title()
-
-//        val browseExistingDownloadsButton = ButtonWidget.Builder(Text.of("Browse Existing Downloads"), { buttonWidget ->
-//            this.client?.setScreen(BrowseDownloadsScreen())
-//        }).build()
-//        browseExistingDownloadsButton.x = 20
-//        browseExistingDownloadsButton.y = 30
-//        addDrawableChild(browseExistingDownloadsButton)
-
-        val worldNameWidget = textFieldWidget(title)
-        downloadButton(worldNameWidget)
-        cancelButton()
-    }
-
-    private fun title(): TextWidget {
         val title = TextWidget(Text.translatable("gui.title", Text.translatable("worldtools")), textRenderer)
-        title.x = width / 2 - title.width / 2
-        title.y = 10
+        SimplePositioningWidget.setPos(title, 0, 0, this.width, this.height, 0.5f, 0.01f)
         addDrawableChild(title)
-        return title
-    }
 
-    private fun cancelButton() {
+        val entryGridWidget = GridWidget()
+        entryGridWidget.mainPositioner.margin(4, 4, 4, 0)
+        val adder = entryGridWidget.createAdder(3)
+        val worldNameTextEntryWidget = TextFieldWidget(textRenderer, 0, 0, 250, 20, Text.of(levelName))
+        worldNameTextEntryWidget.setPlaceholder(Text.translatable("gui.world_name_placeholder", levelName))
+        worldNameTextEntryWidget.setMaxLength(64)
+        adder.add(worldNameTextEntryWidget, 2)
+        adder.add(ButtonWidget.Builder(Text.translatable("gui.start_download")) { _ ->
+            CaptureManager.start(worldNameTextEntryWidget.text)
+            client?.setScreen(null)
+        }.width(90).build(), 1)
+        entryGridWidget.refreshPositions()
+        SimplePositioningWidget.setPos(entryGridWidget, 0, title.y, this.width, this.height, 0.5f, 0.05f)
+        entryGridWidget.forEachChild(this::addDrawableChild)
+
         val cancelButton = ButtonWidget.Builder(Text.translatable("gui.cancel")) { _ ->
             client?.setScreen(null)
         }.build()
-        cancelButton.x = width / 2 - cancelButton.width / 2
-        cancelButton.y = height - cancelButton.height - 40
+        SimplePositioningWidget.setPos(cancelButton, 0, 0, this.width, this.height, 0.5f, 0.95f)
         addDrawableChild(cancelButton)
     }
-
-    private fun downloadButton(worldNameWidget: TextFieldWidget) {
-        val startDownloadButton = ButtonWidget.Builder(Text.translatable("gui.start_download")) { _ ->
-            CaptureManager.start(worldNameWidget.text)
-            client?.setScreen(null)
-        }.build()
-        startDownloadButton.x = worldNameWidget.x + worldNameWidget.width + 10
-        startDownloadButton.y = worldNameWidget.y
-        startDownloadButton.width = 90
-        addDrawableChild(startDownloadButton)
-    }
-
-    private fun textFieldWidget(title: TextWidget): TextFieldWidget {
-        val worldNameWidget = TextFieldWidget(textRenderer, 0, 0, 100, 20, Text.of(levelName))
-        worldNameWidget.setPlaceholder(Text.translatable("gui.world_name_placeholder", levelName))
-        worldNameWidget.setMaxLength(64)
-        worldNameWidget.width = width - 150
-        worldNameWidget.x = 20
-        //        worldNameWidget.y = browseExistingDownloadsButton.y + browseExistingDownloadsButton.height + 20
-        worldNameWidget.y = title.y + title.height + 20
-        addDrawableChild(worldNameWidget)
-        return worldNameWidget
-    }
 }
+
