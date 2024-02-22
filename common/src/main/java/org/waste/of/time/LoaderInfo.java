ed93c17f06548bcf0e17c87868919b6e0a1be435
Constructor <fractalminds@protonmail.com>
2024-02-22 01:48:46 +0100

    Nonnull contract

--- common/src/main/java/org/waste/of/time/LoaderInfo.java
+++ common/src/main/java/org/waste/of/time/LoaderInfo.java
@@ -1,10 +1,13 @@
 package org.waste.of.time;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 public class LoaderInfo {
+    @Contract(pure = true)
     @ExpectPlatform
-    public static String getVersion() {
+    public static @NotNull String getVersion() {
         return "DEV";
     }
 }
