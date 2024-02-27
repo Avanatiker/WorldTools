f9e68d0a538899be1fe66bf680377f60f5d3fd07
Constructor <fractalminds@protonmail.com>
2024-02-27 22:36:13 +0100

    Fix experimental screen mixin

--- common/src/main/java/org/waste/of/time/mixin/IntegratedServerLoaderMixin.java
+++ common/src/main/java/org/waste/of/time/mixin/IntegratedServerLoaderMixin.java
@@ -2,8 +2,9 @@ package org.waste.of.time.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.client.gui.screen.Screen;
+import com.mojang.serialization.Dynamic;
 import net.minecraft.server.integrated.IntegratedServerLoader;
+import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.waste.of.time.WorldTools;
@@ -11,14 +12,14 @@ import org.waste.of.time.WorldTools;
 @Mixin(IntegratedServerLoader.class)
 public class IntegratedServerLoaderMixin {
 
-    @WrapOperation(method = "start(Lnet/minecraft/client/gui/screen/Screen;Ljava/lang/String;)V",
+    @WrapOperation(method = "start(Lnet/minecraft/world/level/storage/LevelStorage$Session;Ljava/lang/Runnable;)V",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/server/integrated/IntegratedServerLoader;start(Lnet/minecraft/client/gui/screen/Screen;Ljava/lang/String;ZZ)V"
+                    target = "Lnet/minecraft/server/integrated/IntegratedServerLoader;start(Lnet/minecraft/world/level/storage/LevelStorage$Session;Lcom/mojang/serialization/Dynamic;ZZLjava/lang/Runnable;)V"
             ))
-    public void disableExperimentalWorldSettingsScreen(IntegratedServerLoader instance, Screen parent, String levelName, boolean safeMode, boolean canShowBackupPrompt, Operation<Void> original) {
+    public void disableExperimentalWorldSettingsScreen(IntegratedServerLoader instance, LevelStorage.Session session, Dynamic<?> levelProperties, boolean safeMode, boolean canShowBackupPrompt, Runnable onCancel, Operation<Void> original) {
         if (WorldTools.INSTANCE.getConfig().getAdvanced().getHideExperimentalWorldGui())
             canShowBackupPrompt = false;
-        original.call(instance, parent, levelName, safeMode, canShowBackupPrompt);
+        original.call(instance, session, levelProperties, safeMode, canShowBackupPrompt, onCancel);
     }
 }
