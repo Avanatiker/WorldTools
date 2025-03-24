385a6e4e1eb85b578c7a26bc9b2228226602fbf8
ts <124226059+uniformization@users.noreply.github.com>
2025-03-24 10:43:51 -0700

    Update HotCache.kt

--- common/src/main/kotlin/org/waste/of/time/storage/cache/HotCache.kt
+++ common/src/main/kotlin/org/waste/of/time/storage/cache/HotCache.kt
@@ -73,9 +73,7 @@ object HotCache {
      */
     @Deprecated("This method will default to the current dimension. Please use the new method by passing in a dimension.")
     @Suppress("unused")
-    fun isChunkSaved(chunkX: Int, chunkZ: Int): Boolean {
-        return isChunkSaved(chunkX, chunkZ, mc.world?.registryKey!!)
-    }
+    fun isChunkSaved(chunkX: Int, chunkZ: Int) = isChunkSaved(chunkX, chunkZ, mc.world?.registryKey!!)
 
     /**
      * Used as a public API for external mods like [XaeroPlus](https://github.com/rfresh2/XaeroPlus), change carefully.
